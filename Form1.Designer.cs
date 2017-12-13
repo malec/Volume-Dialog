e5616ad4ecf398194d0861e713ff78a30582f6cc
AAhlbrandt <alec@ahlbrandt.net>
2017-12-13 00:45:28 -0600

    Working!

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace VolumeDialog___Windows_Forms {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.hideTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.fadeOutTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(349, 19);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // hideTimer
+            // 
+            this.hideTimer.Interval = 1000;
+            this.hideTimer.Tick += new System.EventHandler(this.hideTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(369, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "100";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fadeOutTimer
+            // 
+            this.fadeOutTimer.Tick += new System.EventHandler(this.fadeTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(398, 43);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer hideTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer fadeOutTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+
+namespace VolumeDialog___Windows_Forms {
+    public partial class Form1 : Form {
+        delegate void setVolumeCallback();
+        private Queue<int> volumeQueue;
+        public Form1() {
+            InitializeComponent();
+            volumeQueue = new Queue<int>();
+            var enumer = new MMDeviceEnumerator();
+            var device = enumer.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            var currentVolume = (int)(100*device.AudioEndpointVolume.MasterVolumeLevelScalar);
+            if (volumeInRange(currentVolume)) {
+                progressBar1.Value = currentVolume;
+                label1.Text = currentVolume.ToString();
+            }
+            else {
+                throw new IndexOutOfRangeException("Volume in a bad state. Naudio thinks volume is at " + currentVolume.ToString());
+            }
+            device.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification;
+            hideTimer.Start();
+        }
+        private void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data) {
+            int volume = Convert.ToInt32(Math.Floor(100 * data.MasterVolume));
+            if (volumeInRange(volume)) {
+                volumeQueue.Enqueue(volume);
+                if (progressBar1.InvokeRequired) {
+                    setVolumeCallback cb = new setVolumeCallback(setPBarValue);
+                    this.Invoke(cb);
+                }
+                else {
+                    setPBarValue();
+                }
+            }
+            else {
+                throw new IndexOutOfRangeException("Bad volume. Data is " + volume);
+            }
+        }
+        private void setPBarValue() {
+            Opacity = 100;
+            Show();
+            BringToFront();
+            int volume = volumeQueue.Dequeue();
+            label1.Text = volume.ToString();
+            hideTimer.Stop();
+            progressBar1.Value = volume;
+            hideTimer.Start();
+        }
+
+        private void hideTimer_Tick(object sender, EventArgs e) {
+            fadeOutTimer.Start();
+        }
+
+        private bool volumeInRange(int volume) {
+            if (volume >= 0 && volume <= 100) {
+                return true;
+            }
+            return false;
+        }
+
+        private void fadeTimer_Tick(object sender, EventArgs e) {
+            if (Opacity >= .05) {
+                Opacity -= .05;
+            }
+            else {
+                Hide();
+                fadeOutTimer.Stop();
+            }
+        }
+    }
+}
